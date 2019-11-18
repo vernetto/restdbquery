7128f216fbe50d9f96722f93fcc8e165324950df
Pierluigi Vernetto <publicpierre@gmail.com>
2019-11-18 14:08:37 +0100

    Create RestApp.java

---
+++ RestApp.java
@@ -0,0 +1,26 @@
+package com.csg.query.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/rest")
+public class RestApp extends Application {
+    private Set<Object> singletons = new HashSet<>();
+    private Set<Class<?>> empty = new HashSet<>();
+
+    public RestApp() {
+        singletons.add(new Endpoint());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return empty;
+    }
+}
